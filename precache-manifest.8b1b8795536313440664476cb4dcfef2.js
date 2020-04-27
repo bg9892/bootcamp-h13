0ddb82acf05dcd44765e1f2efce5ca3d41acec87
Bradley <bg9892@gmail.com>
2020-04-27 11:45:20 -0500

    Updates

---
+++ precache-manifest.8b1b8795536313440664476cb4dcfef2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efbe147bb41c079b7251787a17e2fd94",
+    "url": "/user_directory/index.html/index.html"
+  },
+  {
+    "revision": "bdbc8c6c75d4677e65df",
+    "url": "/user_directory/index.html/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "075b4a9d0034a37b7d13",
+    "url": "/user_directory/index.html/static/js/2.20145509.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/user_directory/index.html/static/js/2.20145509.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdbc8c6c75d4677e65df",
+    "url": "/user_directory/index.html/static/js/main.d06b8afd.chunk.js"
+  },
+  {
+    "revision": "fd5c08e568c6f230675e",
+    "url": "/user_directory/index.html/static/js/runtime-main.4ccae029.js"
+  }
+]);
